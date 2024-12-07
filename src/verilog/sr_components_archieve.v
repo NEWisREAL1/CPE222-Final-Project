03896e3f2ef09671fb40efdc4a165223d3d83974
NEWisREAL <acc.newisreal@gmail.com>
2024-12-07 18:29:06 +0700

    PORT.2-2

--- src/verilog/sr_components_archieve.v
+++ src/verilog/sr_components_archieve.v
@@ -44,9 +44,108 @@ module LOAD_DATA # (
     end
 endmodule
 
-module LOAD_CUBE_DATA # (
-    GRAPHICS_FILE = ""
-) (
+module LOAD_DATA_CHAIN(
+    input           master_clk,
+    input           plane_clk,
+    input           frame_clk,
+    input           manual_reset, // short `manual_reset` with low-active `OE` pin on shift register
+    output          SHCP,
+    output          STCP,
+    output          MR,
+    output          DS,
+    output          finish,
+);
+    reg reset;
+    reg ready, frame_changed;
+    reg [15:0] r_addr;
+    wire [6:0] r_data_0;
+    wire [6:0] r_data_1;
+    wire [6:0] r_data_2;
+    wire [6:0] r_data_3;
+    wire [6:0] r_data_4;
+    wire [6:0] r_data_5;
+    wire [6:0] r_data_6;
+    wire [55:0] r_data_buffer;
+    wire [55:0] r_data_invert;
+    wire [55:0] r_data;
+    reg [1:0] current_color;
+    reg [2:0] current_plane;
+
+    ROM #(.INIT_FILE("mem_plane_test.mem"), .READ_MODE("B")) MEM1(
+        .r_addr(r_addr),
+        .r_data_0(r_data_0),
+        .r_data_1(r_data_1),
+        .r_data_2(r_data_2),
+        .r_data_3(r_data_3),
+        .r_data_4(r_data_4),
+        .r_data_5(r_data_5),
+        .r_data_6(r_data_6)
+    );
+
+    LOAD_DATA #(.DATA_WIDTH(56)) SL1(
+        .clk(master_clk),
+        .data(r_data),
+        .reset(reset || manual_reset),
+        .DS(DS),
+        .SHCP(SHCP),
+        .STCP(STCP),
+        .MR(MR),
+        .finish(finish)
+    );
+
+    initial begin
+        ready = 0; current_color = 0;
+    end
+
+    always @ (posedge frame_clk) begin
+        if (manual_reset) begin
+            ready <= 0;
+            r_addr <= 0;
+            reset <= 1;
+        end
+        else begin
+            if (ready) begin
+                ready <= 0;
+                reset <= 0;
+                if (r_addr == 140) begin // ADJUST THIS NUMBER TO MATCH THE NUMBER OF FRAME
+                    r_addr <= 0;
+                end
+                else begin
+                    r_addr <= r_addr + 21;
+                end
+            end
+            else begin
+                reset <= 1;
+                ready <= 1;
+            end
+        end
+    end
+
+    assign r_data_buffer =  {1'b0, r_data_0, 1'b0, r_data_1, 1'b0, r_data_2, 1'b0, r_data_3, 1'b0, r_data_4, 1'b0, r_data_5, 1'b0, r_data_6};
+    assign r_data_invert = ~{1'b1, r_data_0, 1'b1, r_data_1, 1'b1, r_data_2, 1'b1, r_data_3, 1'b1, r_data_4, 1'b1, r_data_5, 1'b1, r_data_6};
+    assign r_data = current_color == 0 ? r_data_invert : r_data_buffer;
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module LOAD_CUBE_DATA (
     input           master_clk,
     input           color_clk,
     input           plane_clk,
@@ -63,10 +162,10 @@ module LOAD_CUBE_DATA # (
 );
     reg reset, ready;
     reg color_changed, plane_changed, frame_changed;
-    wire color_shift_finish, control_shift_finish;
-    wire [15:0] r_addr;
+    reg color_shift_finish, control_shift_finish;
+    reg [15:0] r_addr;
     wire [6:0] r_data_0, r_data_1, r_data_2, r_data_3, r_data_4, r_data_5, r_data_6;
-    reg [6:0] red_data, green_data, blue_data;
+    wire [6:0] red_data, green_data, blue_data;
     wire [55:0] control_data_buffer;
     wire [55:0] control_data_invert;
     wire [55:0] control_data;
@@ -80,7 +179,7 @@ module LOAD_CUBE_DATA # (
 
     reg [6:0] R_data, G_data, B_data;
 
-    ROM #(.INIT_FILE(GRAPHICS_FILE), .READ_MODE("B")) MEM1(
+    ROM #(.INIT_FILE("mem_plane_test.mem"), .READ_MODE("B")) MEM1(
         .r_addr(r_addr),
         .r_data_0(r_data_0),
         .r_data_1(r_data_1),
@@ -108,20 +207,20 @@ module LOAD_CUBE_DATA # (
         .reset(reset || manual_reset),
         .DS(COLOR_DS),
         .SHCP(COLOR_SHCP),
-        //.STCP(STCP),
-        //.MR(MR),
+        .STCP(STCP),
+        .MR(MR),
         .finish(color_shift_finish)
     );
 
     initial begin
-        ready = 0;
+        ready = 0; r_addr = 0;
         current_color = 0; current_plane = 0; current_frame = 0;
     end
 
     always @ (posedge color_clk) begin
         previous_color <= current_color;
         if (current_color == 2'b10) begin
-            current_color <= 0;
+            current_color <= 0
         end
         else begin
             current_color <= current_color + 1;
@@ -141,7 +240,7 @@ module LOAD_CUBE_DATA # (
     always @ (posedge plane_clk) begin
         previous_plane <= current_plane;
         if (current_plane == 3'b111) begin
-            current_plane <= 0;
+            current_plane <= 0
         end
         else begin
             current_plane <= current_plane + 1;
@@ -151,62 +250,15 @@ module LOAD_CUBE_DATA # (
     always @ (posedge frame_clk) begin
         previous_frame <= current_plane;
         if (current_frame == frame_number) begin
-            current_frame <= 0;
+            current_frame <= 0
         end
         else begin
             current_frame <= current_frame + 1;
         end
     end
     
-    assign r_addr = (current_color * 7) + (current_plane * 21) + (current_frame * 147);
     assign control_data_buffer =  {1'b0, r_data_0, 1'b0, r_data_1, 1'b0, r_data_2, 1'b0, r_data_3, 1'b0, r_data_4, 1'b0, r_data_5, 1'b0, r_data_6};
     assign control_data_invert = ~{1'b1, r_data_0, 1'b1, r_data_1, 1'b1, r_data_2, 1'b1, r_data_3, 1'b1, r_data_4, 1'b1, r_data_5, 1'b1, r_data_6};
     assign control_data = current_color == 0 ? control_data_invert : control_data_buffer;
     assign color_data = {red_data, 1'b0, green_data, 1'b0, blue_data, 1'b0};
-endmodule
-
-module top_module (
-    input           master_clk,
-    output          R_DS,
-    output          G_DS,
-    output          B_DS,
-    output          CONTROL_DS,
-    output          COLOR_SHCP,
-    output          CONTROL_SHCP,
-    output          STCP,
-    output          MR
-);
-    wire color_clk, plane_clk, frame_clk;
-
-    FREQ_DIVIDER # (.DIV_WIDTH(32)) DIV1(
-        .in_clk(master_clk),
-        .divider(6'd50000),
-        .out_clk(color_clk)
-    );
-    FREQ_DIVIDER # (.DIV_WIDTH(32)) DIV2(
-        .in_clk(master_clk),
-        .divider(9'd500000),
-        .out_clk(plane_clk)
-    );
-    FREQ_DIVIDER # (.DIV_WIDTH(32)) DIV3(
-        .in_clk(master_clk),
-        .divider(13'd500000),
-        .out_clk(frame_clk)
-    );
-
-    LOAD_CUBE_DATA # (.GRAPHICS_FILE("mem_cube_test.mem")) LOADER(
-        .master_clk(master_clk),
-        .color_clk(color_clk),
-        .plane_clk(plane_clk),
-        .frame_clk(frame_clk),
-        .frame_number(2),
-        .R_DS(R_DS),
-        .G_DS(G_DS),
-        .B_DS(B_DS),
-        .CONTROL_DS(CONTROL_DS),
-        .COLOR_SHCP(COLOR_SHCP),
-        .CONTROL_SHCP(CONTROL_SHCP),
-        .STCP(STCP),
-        .MR(MR)
-    );
 endmodule
